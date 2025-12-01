b7d9907067431c3762025f016cd75062e6143645
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2025-12-01 23:26:35 +0600

    Solve Assignment_2 Task_01.

---
+++ Assignment_2/Task_01/Program.cs
@@ -0,0 +1,22 @@
+﻿
+string input = Console.ReadLine();
+
+string[] parts = input.Split(' ');
+
+int x = int.Parse(parts[0]);
+int y = int.Parse(parts[1]);
+int z = int.Parse(parts[2]);
+int mod = (x % y);
+string[] array = new string[z];
+
+for (int i = 0; i < z; i++)
+{
+    while (mod < y)
+    {
+        mod = mod * 10;
+    }
+    array[i] = (mod / 7).ToString();
+    mod = mod % y;
+}
+string result = string.Join("", array);
+Console.WriteLine($"{x / y}.{result}");
