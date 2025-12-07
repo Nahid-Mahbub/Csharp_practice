3df0b8899659556059ad5e91851aebf2773f0dc2
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2025-12-07 10:18:26 +0600

    Solve the problem bit++

---
+++ Assignment_1/282A - Bit++/Program.cs
@@ -0,0 +1,24 @@
+﻿string index = Console.ReadLine();
+int n = int.Parse(index);
+int answer = 0;
+for (int i = 0; i < n; i++)
+{
+    string input = Console.ReadLine();
+    if (input == "++X")
+    {
+        answer++;
+    }
+    else if (input == "--X")
+    {
+        answer--;
+    }
+    else if (input == "X--")
+    {
+        answer--;
+    }
+    else if (input == "X++")
+    {
+        answer++;
+    }
+}
+Console.WriteLine($"{answer}");
