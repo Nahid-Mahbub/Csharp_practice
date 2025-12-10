655a2e5ee9edfa84c05850c9fe891ed5f4605d9a
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2025-12-10 20:50:57 +0600

    Solve the problem 71A - Way Too Long Words

---
+++ Assignment_1/71A - Way Too Long Words/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+
+string input = Console.ReadLine();
+int index = int.Parse(input);
+string[] Loop_input = new string[index];
+//Console.WriteLine(index);
+//Console.WriteLine(index.GetType());
+
+for (int i = 0; i < index; i++)
+{
+    Loop_input[i] = Console.ReadLine();
+}
+for (int i = 0; i < index; i++)
+{
+    if (Loop_input[i].Length > 10)
+    {
+        Console.WriteLine($"{Loop_input[i][0]}{Loop_input[i].Length - 2}{Loop_input[i][Loop_input[i].Length - 1]}");
+    }
+    else
+    {
+        Console.WriteLine($"{Loop_input[i]}");
+    }
+}
