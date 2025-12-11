13026ad3ae445f075612086bace9d7c82a54a445
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2025-12-11 10:12:51 +0600

    546A - Soldier and Bananas

---
+++ Assignment_1/546A - Soldier and Bananas/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] input = Console.ReadLine().Split();
+int price  = int.Parse(input[0]);
+int dollars  = int.Parse(input[1]);
+int banana = int.Parse(input[2]);
+int fullPrice = 0;
+
+for  (int i = 1; i <= banana; i++)
+{
+    fullPrice += price * i;
+}
+if (fullPrice - dollars > 0)
+{
+    Console.WriteLine(fullPrice - dollars);
+}
+else
+{
+    Console.WriteLine(0);
+}
