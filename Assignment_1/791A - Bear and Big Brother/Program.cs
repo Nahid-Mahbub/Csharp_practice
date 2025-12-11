a0d07122e737fbd11cdbe619cb775f0f968275af
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2025-12-11 10:23:08 +0600

    791A - Bear and Big Brother

---
+++ Assignment_1/791A - Bear and Big Brother/Program.cs
@@ -0,0 +1,11 @@
+﻿string[] input = Console.ReadLine().Split();
+var a = int.Parse(input[0]);
+var b = int.Parse(input[1]);
+int counter = 0;
+while (a <= b)
+{
+    a *= 3;
+    b *= 2;
+    counter++;
+}
+Console.WriteLine(counter);
