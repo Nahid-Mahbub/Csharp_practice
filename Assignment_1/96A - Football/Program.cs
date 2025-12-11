b8d597a795f95e054e4863e585747774658321db
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2025-12-11 09:59:46 +0600

    96A - Football

---
+++ Assignment_1/96A - Football/Program.cs
@@ -0,0 +1,22 @@
+﻿string input = Console.ReadLine();
+var current = input[0];
+var counter = 1;
+for (int i = 1; i < input.Length; i++)
+{
+    if (current == input[i])
+    {
+        counter++;
+    }
+    else
+    {
+        counter = 1;
+        current = input[i];
+    }
+
+    if (counter == 7)
+    {
+        Console.Write("Yes");
+        return;
+    }
+}
+Console.Write("No");
