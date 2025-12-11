10f0227ecd6cb323265c424802207e5bb51ecf2f
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2025-12-11 09:27:26 +0600

    Solve problem 266A - Stones on the Table

---
+++ Assignment_1/266A - Stones on the Table/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+
+string input = Console.ReadLine();
+var stones = int.Parse(input);
+var counter = 0;
+string colors = Console.ReadLine();
+
+for (int i = 0; i < stones - 1; i++)
+{
+    if (colors[i] == colors[i + 1])
+    {
+        counter++;
+    }
+}
+Console.WriteLine(counter);
