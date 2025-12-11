bcdbdc90ce9d12744bf79efe5aa75eab94ab344d
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2025-12-11 09:46:26 +0600

    236A - Boy or Girl

---
+++ Assignment_1/236A - Boy or Girl/Program.cs
@@ -0,0 +1,15 @@
+﻿string input = Console.ReadLine();
+var set = new HashSet<char>();
+
+for  (int i = 0; i < input.Length; i++)
+{
+    set.Add(input[i]);
+}
+if(set.Count % 2 == 0)
+{
+    Console.WriteLine("CHAT WITH HER!");
+}
+else
+{
+    Console.WriteLine("IGNORE HIM!");
+}
