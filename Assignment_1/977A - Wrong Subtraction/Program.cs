242fa9f3e91f8a9eb41b797e12e66cedd743981f
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2025-12-12 10:34:35 +0600

    977A - Wrong Subtraction

---
+++ Assignment_1/977A - Wrong Subtraction/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] input = Console.ReadLine().Split();
+
+int val = int.Parse(input[0]);
+int k = int.Parse(input[1]);
+
+for (int i = 0; i < k; i++)
+{
+    if (val % 10 != 0)
+    {
+        val--;
+    }
+    else
+    {
+        val /= 10;
+    }
+}
+Console.WriteLine(val);
