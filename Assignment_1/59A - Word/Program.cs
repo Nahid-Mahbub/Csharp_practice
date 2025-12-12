34e18f9c3034ba36d606d51fd83879ac39348195
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2025-12-12 11:42:35 +0600

    59A - Word

---
+++ Assignment_1/59A - Word/Program.cs
@@ -0,0 +1,40 @@
+﻿string input = Console.ReadLine();
+int capitalCounter = 0;
+int smallCounter = 0;
+char[] arr = input.ToCharArray();
+
+for  (int i = 0; i < input.Length; i++)
+{
+    if (input[i] < 'a')
+    {
+        capitalCounter++;
+    }
+    else
+    {
+        smallCounter++;
+    }
+}
+
+if(capitalCounter > smallCounter)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 'Z')
+        {
+            arr[i] = (char)((int)(arr[i]) - 32);
+        }
+    }
+}
+else
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < 'a')
+        {
+            arr[i] = (char)((int)(arr[i]) + 32);
+        }
+    }
+        
+}
+string output = new string(arr);
+Console.WriteLine(output);
