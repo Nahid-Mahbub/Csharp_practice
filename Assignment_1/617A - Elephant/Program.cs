97ebc37ef03e2f886efe7c17d364f123ab959e52
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2025-12-12 10:57:08 +0600

    617A - Elephant

---
+++ Assignment_1/617A - Elephant/Program.cs
@@ -0,0 +1,32 @@
+﻿string input = Console.ReadLine();
+int num = int.Parse(input);
+int counter = 0;
+while (num != 0)
+{
+    if (num >= 5)
+    {
+        counter += (num/5);
+        num %= 5;
+    }
+    else if (num >= 4)
+    {
+        counter += (num/4);
+        num %= 4;
+    }
+    else if (num >= 3)
+    {
+        counter += (num/3);
+        num %= 3;
+    }
+    else if (num >= 2)
+    {
+        counter += (num/2);
+        num %= 2;
+    }
+    else
+    {
+        num %= 1;
+        counter++;
+    }
+}
+Console.WriteLine(counter);
