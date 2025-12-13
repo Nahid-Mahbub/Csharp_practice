8389b64325fa10b779787338a3e35b477c8b633f
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2025-12-13 08:33:23 +0600

    116A - Tram

---
+++ Assignment_1/116A - Tram/Program.cs
@@ -0,0 +1,17 @@
+﻿string input = Console.ReadLine();
+int loop = int.Parse(input);
+int counter = 0;
+int max = 0;
+for (int i = 0; i < loop; i++)
+{
+    string[] rowInput = Console.ReadLine().Split();
+    int a = int.Parse(rowInput[0]);
+    int b = int.Parse(rowInput[1]);
+    counter = counter - a;
+    counter = counter + b;
+    if (max <  counter)
+    {
+        max = counter;
+    }
+}
+Console.WriteLine(max);
