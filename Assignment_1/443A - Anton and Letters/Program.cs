078abe56ce2f665545f68e37f3b92cb8e1ab2a21
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-01-12 12:53:56 +0600

    443A - Anton and Letters

---
+++ Assignment_1/443A - Anton and Letters/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+HashSet<char> newSet = new HashSet<char>();
+string input = Console.ReadLine();
+
+input = input.Trim('{', '}');
+
+foreach (char c in input)
+{
+    if (char.IsLetter(c))
+    {
+        newSet.Add(c);
+    }
+}
+
+Console.WriteLine(newSet.Count);
