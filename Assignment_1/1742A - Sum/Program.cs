faa25e0f8ee4a9b698c8b40e861bb4d4a68b82d4
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-01-12 12:22:46 +0600

    1742A - Sum

---
+++ Assignment_1/1742A - Sum/Program.cs
@@ -0,0 +1,19 @@
+﻿int loop = int.Parse(Console.ReadLine());
+for (int i = 0; i < loop; i++)
+{
+    int[] array = new int[3];
+    string input = Console.ReadLine();
+    string[] slice = input.Split(" ");
+    array[0] = int.Parse(slice[0]);
+    array[1] = int.Parse(slice[1]);
+    array[2] = int.Parse(slice[2]);
+    Array.Sort(array);
+    if (array[0] +  array[1] == array[2])
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+}
