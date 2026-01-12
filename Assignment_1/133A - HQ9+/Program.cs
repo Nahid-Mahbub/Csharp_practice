3fd147066a3069f5f1ddc0e61233a9aacb91147d
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-01-12 11:38:30 +0600

    133A - HQ9+

---
+++ Assignment_1/133A - HQ9+/Program.cs
@@ -0,0 +1,10 @@
+﻿string input = Console.ReadLine();
+foreach (char c in input)
+{
+    if (c == 'H' || c == 'Q' || c == '9')
+    {
+        Console.WriteLine("YES");
+        return;
+    }
+}
+Console.WriteLine("NO");
