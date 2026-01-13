408f78544c8045e54522036996faba9bf5e8b15b
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-01-13 19:36:16 +0600

    Practice Ado.net

---
+++ Practice_01/AdonetExample/Program.cs
@@ -0,0 +1,9 @@
+﻿using AdonetExample;
+
+var connectionString = "Server=.\\SQLEXPRESS;Database=CSharpB22;User Id=CSharpB22;Password=624282;Trust Server Certificate = true";
+
+var sql = "insert into students (name, CGPA, dateofbirth, isActive) values ('Naimur', 3.5, '2000-01-01', 1)";
+
+SqlUtility utility = new SqlUtility(connectionString);
+utility.ExecuteSQL(sql);
+Console.WriteLine("Done");
---
+++ Practice_01/AdonetExample/SqlUtility.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.SqlClient;
+
+
+namespace AdonetExample
+{
+    public class SqlUtility
+    {
+        private readonly string _connectionString;
+
+        // Constructor
+        public SqlUtility(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        // Method
+        public void ExecuteSQL(string sql)
+        {
+            
+            using SqlConnection connection = new SqlConnection(_connectionString);
+
+            if(connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+
+            using SqlCommand command = new SqlCommand(sql, connection);
+           
+            int rowsAffected = command.ExecuteNonQuery();
+            Console.WriteLine(rowsAffected);
+
+            connection.Close();
+        }
+    }
+}
