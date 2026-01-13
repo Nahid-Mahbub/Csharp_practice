ca4fba3cf630e09d9ab7b5a93130b16b21af1ad0
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-01-13 12:39:21 +0600

    1154A - Restoring Three Numbers

---
+++ Assignment_1/1154A - Restoring Three Numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] input = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToArray();
+Array.Sort(input);
+
+Console.Write(input[3] - input[0]);
+Console.Write(" ");
+Console.Write(input[3] - input[1]);
+Console.Write(" ");
+Console.Write(input[3] - input[2]);
