0bf211cb5706f7dce59dcfc79969cd0d1184b44e
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-01-14 11:12:17 +0600

    1335A - Candies and Two Sisters

---
+++ Assignment_1/1335A - Candies and Two Sisters/Program.cs
@@ -0,0 +1,21 @@
+﻿int loop = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < loop; i++)
+{
+    int num = int.Parse(Console.ReadLine());
+    if (num < 3)
+    {
+        Console.WriteLine('0');
+    }
+    else
+    {
+        if (num % 2 == 0)
+        {
+            Console.WriteLine(num / 2 - 1);
+        }
+        else
+        {
+            Console.WriteLine(num / 2);
+        }
+    }
+}
