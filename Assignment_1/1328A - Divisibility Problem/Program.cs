4ff259992776e800d04dfd65b085abc96af7cead
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-01-14 10:53:33 +0600

    1328A - Divisibility Problem

---
+++ Assignment_1/1328A - Divisibility Problem/Program.cs
@@ -0,0 +1,21 @@
+﻿int input = int.Parse(Console.ReadLine());
+for (int i = 0; i < input; i++)
+{
+    int[] ints = Console.ReadLine()
+        .Split(' ')
+        .Select(int.Parse)
+        .ToArray();
+
+    if (ints[0] % ints[1] == 0)
+    {
+        Console.WriteLine('0');
+    }
+    else if(ints[0] < ints[1])
+    {
+        Console.WriteLine(ints[1] - ints[0]);
+    }
+    else
+    {
+        Console.WriteLine(ints[1] - (ints[0] % ints[1]));
+    }
+}
