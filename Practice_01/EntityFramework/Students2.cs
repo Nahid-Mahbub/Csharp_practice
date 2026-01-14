1bb4cd0afca6ed8ec3bfca84697fc2a3fe022995
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-01-14 22:21:35 +0600

    Practice EntityFramework/Students2.cs

--- Practice_01/EntityFramework/Students2.cs
+++ Practice_01/EntityFramework/Students2.cs
@@ -4,12 +4,26 @@ using System.Text;
 
 namespace EntityFramework
 {
+    // এই ক্লাসটি ডাটাবেসের Students2 টেবিলকে represent করে
+    // Entity Framework এই ক্লাসের মাধ্যমে টেবিলের structure বুঝে নেয়
     public class Students2
     {
+        // এই property টি টেবিলের Primary Key
+        // EF Core নিজে থেকেই এটাকে IDENTITY হিসেবে সেট করে
         public int Id { get; set; }
+
+        // শিক্ষার্থীর নাম সংরক্ষণ করার জন্য
+        // এটি ডাটাবেসে nvarchar টাইপ হিসেবে থাকবে
         public string Name { get; set; }
+
+        // শিক্ষার্থীর CGPA সংরক্ষণ করার জন্য
         public double CGPA { get; set; }
+
+        // শিক্ষার্থীর জন্ম তারিখ সংরক্ষণ করার জন্য
         public DateTime DateOfBirth { get; set; }
+
+        // শিক্ষার্থী বর্তমানে active কিনা তা বোঝানোর জন্য
+        // true = active, false = inactive
         public bool IsActive { get; set; }
     }
 }
--- Practice_01/EntityFramework/TrainingDbContext.cs
+++ Practice_01/EntityFramework/TrainingDbContext.cs
@@ -1,18 +1,33 @@
-﻿
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace EntityFramework
 {
+    // এই ক্লাসটি হচ্ছে আমাদের Database Context
+    // DbContext এর মাধ্যমে Entity Framework ডাটাবেসের সাথে যোগাযোগ করে
     public class TrainingDbContext : DbContext
     {
+        // এই DbSet টি Students2 টেবিলকে represent করে
+        // Database এ Students2 নামে একটি টেবিল থাকবে
         public DbSet<Students2> Students2 { get; set; }
+
+        // এই মেথডটি ডাটাবেসের connection configure করার জন্য ব্যবহার হয়
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if(!optionsBuilder.IsConfigured)
-                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=CSharpB22;User Id=CSharpB22;Password=624282;Trust Server Certificate = true");
+            // যদি আগে থেকে কোন configuration না থাকে
+            if (!optionsBuilder.IsConfigured)
+            {
+                // এখানে SQL Server এর connection string দেওয়া হয়েছে
+                // Server = SQL Server instance
+                // Database = কোন ডাটাবেস ব্যবহার হবে
+                // User Id & Password = authentication তথ্য
+                // Trust Server Certificate = certificate error avoid করার জন্য
+                optionsBuilder.UseSqlServer(
+                    "Server=.\\SQLEXPRESS;Database=CSharpB22;User Id=CSharpB22;Password=624282;Trust Server Certificate=true"
+                );
+            }
         }
     }
 }
