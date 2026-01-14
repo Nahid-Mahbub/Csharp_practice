dc45ec4eed58d01cd36eb26bcc633a043b8258a0
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-01-14 12:07:53 +0600

    1343B - Balanced Array

---
+++ Assignment_1/1343B - Balanced Array/Program.cs
@@ -0,0 +1,28 @@
+﻿int loop = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < loop; i++)
+{
+    int num = int.Parse(Console.ReadLine());
+    if ((num / 2) % 2 == 0)
+    {
+        int sumEven = 0;
+        int sumOdd = 0;
+        Console.WriteLine("Yes");
+
+        for (int j = 2; j <= num; j+=2)
+        {
+            sumEven += j;
+            Console.Write($"{j} ");
+        }
+        for (int j = 1, k = 1; k < (num / 2); j += 2, k++)
+        {
+            sumOdd += j;
+            Console.Write($"{j} ");
+        }
+        Console.WriteLine($"{sumEven - sumOdd}");
+    }
+    else
+    {
+        Console.WriteLine("No");
+    }
+}
