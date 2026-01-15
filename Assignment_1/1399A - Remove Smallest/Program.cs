64735ee0effe7ff9f861ec3f9356f4e674a5b164
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-01-15 13:30:31 +0600

    1399A - Remove Smallest

---
+++ Assignment_1/1399A - Remove Smallest/Program.cs
@@ -0,0 +1,20 @@
+﻿int loop = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < loop; i++)
+{
+    int loop2 = int.Parse(Console.ReadLine());
+    int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    Array.Sort(array);
+    bool flag = false;
+    for (int j = 1; j < array.Length; j++)
+    {
+        if (Math.Abs(array[j] - array[j-1]) > 1)
+        {
+            Console.WriteLine("No");
+            flag = true;
+            break;
+        }
+    }
+    if (flag == false)
+        Console.WriteLine("Yes");
+}
