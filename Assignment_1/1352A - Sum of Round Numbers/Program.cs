b1b9e9a8e1255bd29fa9b2922f84f1c8fed03194
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-01-15 12:38:37 +0600

    1352A - Sum of Round Numbers

---
+++ Assignment_1/1352A - Sum of Round Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿int loop = int.Parse(Console.ReadLine());
+for (int i = 0; i < loop; i++)
+{
+    int counter = 0;
+    List<int> list = new List<int>();
+    int number = int.Parse(Console.ReadLine());
+    if (number > 1000)
+    {
+        counter++;
+        list.Add((number/1000) * 1000);
+        number %= 1000;
+    }
+    if (number > 100)
+    {
+        counter++;
+        list.Add((number/100) * 100);
+        number %= 100;
+    }
+    if (number > 9)
+    {
+        counter++;
+        list.Add((number/10) * 10);
+        number %= 10;
+    }
+    if (number < 10 && number != 0)
+    {
+        counter++;
+        list.Add(number);
+    }
+    Console.WriteLine(counter);
+    foreach (var item in list)
+    {
+        Console.Write($"{item} ");
+    }
+    Console.WriteLine();
+}
