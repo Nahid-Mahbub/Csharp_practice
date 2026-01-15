1f0ad2ac3cab275c2e96546fe5c5e48fb27b1252
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-01-15 12:59:21 +0600

    1367A - Short Substrings

---
+++ Assignment_1/1367A - Short Substrings/Program.cs
@@ -0,0 +1,15 @@
+﻿int loop = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < loop; i++)
+{
+    string input = Console.ReadLine();
+    for (int j = 0; j < input.Length; j++)
+    {
+        if(j % 2 == 0 && j != 0)
+        {
+            continue;
+        }
+        Console.Write(input[j]);
+    }
+    Console.WriteLine();
+}
