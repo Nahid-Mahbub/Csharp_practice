e8efefdd95a09a55114304643c5abe652a86bdb6
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-01-17 11:11:01 +0600

    144A - Arrival of the General

---
+++ Assignment_1/144A - Arrival of the General/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading.Channels;
+
+Console.ReadLine();
+
+int[] array = Console.ReadLine()
+    .Split(' ')
+    .Select(int.Parse)
+    .ToArray();
+
+int maxIndex = Array.IndexOf(array, array.Max());
+int minIndex = 0;
+for (int i = array.Length - 1; i >= 0; i--)
+{
+    if (array[i] == array.Min())
+    {
+        minIndex = i;
+        break;
+    }
+}
+
+if (maxIndex < minIndex)
+{
+    Console.WriteLine((maxIndex - 0) + (array.Length-1 - minIndex));
+}
+else
+{
+    Console.WriteLine((maxIndex - 0) + array.Length - 2 - minIndex);
+}
