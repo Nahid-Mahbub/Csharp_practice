253e32671b379cdb915c5d6fed1236faf79551d9
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-01-17 12:09:38 +0600

    1619A - Square String

---
+++ Assignment_1/1619A - Square String/Program.cs
@@ -0,0 +1,25 @@
+﻿int loop = int.Parse(Console.ReadLine());
+for (int i = 0; i < loop; i++)
+{
+    string s = Console.ReadLine();
+    int len = s.Length;
+    if (len % 2 != 0)
+    {
+        Console.WriteLine("NO");
+        continue;
+    }
+    else
+    {
+        string firstHalf = s.Substring(0, len / 2);
+        string secondHalf = s.Substring(len / 2, len / 2);
+        if (firstHalf == secondHalf)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+    }
+
+}
