be01c2c04272d7575bdef5bb693202671a41d5e4
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-01-19 21:40:54 +0600

    1623A - Robot Cleaner, partially solved

---
+++ Assignment_1/1623A - Robot Cleaner/Program.cs
@@ -0,0 +1,52 @@
+﻿int loop = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < loop; i++)
+{
+    int[] array = Console.ReadLine()
+        .Split(' ')
+        .Select(int.Parse)
+        .ToArray();
+    int counter = 0;
+    bool row = true;
+    bool column = true;
+    while (true)
+    {
+        if (array[2] == array[4] || array[3] == array[5])
+        {
+            Console.WriteLine(counter);
+            break;
+        }
+        else
+        {
+            if (array[2] < array[0] && row == true)
+            {
+                array[2]++;
+            }
+            else
+            {
+                row = false;
+                array[2]--;
+                if (array[2] == 0)
+                {
+                    row = true;
+                }
+
+            }
+
+            if (array[3] < array[1] && column == true)
+            {
+                array[3]++;
+            }
+            else
+            {
+                row = false;
+                array[3]--;
+                if (array[3] == 0)
+                {
+                    column = true;
+                }
+            }
+            counter++;
+        }
+    }
+}
